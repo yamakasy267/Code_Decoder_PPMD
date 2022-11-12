a010b5c38a270822d2c25f512f775f0dda3e8429
yamakasy267 <yamakasy267@DESKTOP-BNC6QLP>
2022-11-12 22:32:16 +0300

    Добавьте файлы проекта.

---
+++ coder_and_decoder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static  void Main(string[] args)
+        {
+            string? path = Console.ReadLine();
+            string[] dirs = Directory.GetFiles(path);
+            foreach (string p in dirs)
+            {
+                using (FileStream fstream = File.OpenRead(p))
+                {
+                    byte[] buffer = new byte[fstream.Length];
+                    fstream.Read(buffer, 0, buffer.Length);
+                    byte[] alphabet = buffer.Distinct().ToArray();
+                    string textFromFile = Encoding.Default.GetString(buffer);
+                    Array.ForEach(alphabet, i => Console.WriteLine(i));
+                }
+                break;
+            }
+
+        }
+    }
+}
